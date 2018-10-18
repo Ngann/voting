215ddd42b36f04be2599ad24de14d0f979327f12
Student <example@example.com>
2018-10-18 12:25:31 -0700

    add celebrity match making site

---
+++ js/celebrity.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  //
+  // $("#formOne").submit(function(event) {
+  //   var ageInput = parseInt($("input#age").val());
+  //   var genderInput = $("input#gender").val();
+  //   var nameInput = $("input#name").val();
+  //
+  //   $(".name").text("blah");
+  //   $(".age").text("blah");
+  //   $(".gender").text(genderInput);
+  //
+  //   // $(".match").show();
+  //   console.log(ageInput);
+  //   event.preventDefault();
+  // });
+  alert("hello")
+});
